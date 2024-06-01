f338f5b0043fdcff0827aa1e6bad231e26f03aee
Carlos Peraza <carlosperazavz@gmail.com>
2024-06-01 09:10:22 -0400

    cambio de constante url

--- app/views/inc/head.php
+++ app/views/inc/head.php
@@ -7,26 +7,26 @@
 
 <title><?php echo APP_NAME; ?></title>
 <!-- App favicon -->
-<link rel="shortcut icon" href="http://localhost/multimarket/app/views/images/favicon.ico">
+<link rel="shortcut icon" href="<?php echo APP_URL; ?>app/views/images/favicon.ico">
 
 <!-- jsvectormap css -->
-<link href="http://localhost/multimarket/app/views/libs/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo APP_URL; ?>app/views/libs/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
 
 <!--Swiper slider css-->
-<link href="http://localhost/multimarket/app/views/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo APP_URL; ?>app/views/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
 
 <!-- Layout config Js -->
-<script src="http://localhost/multimarket/app/views/js/layout.js"></script>
+<script src="<?php echo APP_URL; ?>app/views/js/layout.js"></script>
 <!-- Bootstrap Css -->
-<link href="http://localhost/multimarket/app/views/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo APP_URL; ?>app/views/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="http://localhost/multimarket/app/views/css/icons.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo APP_URL; ?>app/views/css/icons.min.css" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="http://localhost/multimarket/app/views/css/app.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo APP_URL; ?>app/views/css/app.min.css" rel="stylesheet" type="text/css" />
 <!-- custom Css-->
-<link href="http://localhost/multimarket/app/views/css/custom.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo APP_URL; ?>app/views/css/custom.min.css" rel="stylesheet" type="text/css" />
 <!-- Sweet Alert css-->
-<link href="http://localhost/multimarket/app/views/libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo APP_URL; ?>app/views/libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
 
 <!--datatable css-->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" />
--- app/views/inc/script.php
+++ app/views/inc/script.php
@@ -1,27 +1,27 @@
 
    <!-- <script src="<php echo APP_URL; ?>app/views/js/main.js" ></script>-->
    <!-- JAVASCRIPT -->
-   <script src="http://localhost/multimarket/app/views/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="http://localhost/multimarket/app/views/libs/simplebar/simplebar.min.js"></script>
-    <script src="http://localhost/multimarket/app/views/libs/node-waves/waves.min.js"></script>
-    <script src="http://localhost/multimarket/app/views/libs/feather-icons/feather.min.js"></script>
-    <script src="http://localhost/multimarket/app/views/js/pages/plugins/lord-icon-2.1.0.js"></script>
-    <script src="http://localhost/multimarket/app/views/js/plugins.js"></script>
+   <script src="<?php echo APP_URL; ?>app/views/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/libs/simplebar/simplebar.min.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/libs/node-waves/waves.min.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/libs/feather-icons/feather.min.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/js/plugins.js"></script>
     <!-- apexcharts -->
-    <script src="http://localhost/multimarket/app/views/libs/apexcharts/apexcharts.min.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/libs/apexcharts/apexcharts.min.js"></script>
     <!-- Vector map-->
-    <script src="http://localhost/multimarket/app/views/libs/jsvectormap/js/jsvectormap.min.js"></script>
-    <script src="http://localhost/multimarket/app/views/libs/jsvectormap/maps/world-merc.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/libs/jsvectormap/js/jsvectormap.min.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/libs/jsvectormap/maps/world-merc.js"></script>
     <!--Swiper slider js-->
-    <script src="http://localhost/multimarket/app/views/libs/swiper/swiper-bundle.min.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/libs/swiper/swiper-bundle.min.js"></script>
     <!-- Dashboard init -->
-    <script src="http://localhost/multimarket/app/views/js/pages/dashboard-ecommerce.init.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/js/pages/dashboard-ecommerce.init.js"></script>
     <!-- profile init js -->
-    <script src="http://localhost/multimarket/app/views/js/pages/profile.init.js"></script>
+    <script src="<?php echo APP_URL; ?>app/views/js/pages/profile.init.js"></script>
    <!-- Sweet Alerts js -->
-   <script src="http://localhost/multimarket/app/views/libs/sweetalert2/sweetalert2.min.js"></script>
+   <script src="<?php echo APP_URL; ?>app/views/libs/sweetalert2/sweetalert2.min.js"></script>
    <!-- Sweet alert init js-->
-   <script src="http://localhost/multimarket/app/views/js/pages/sweetalerts.init.js"></script>
+   <script src="<?php echo APP_URL; ?>app/views/js/pages/sweetalerts.init.js"></script>
 
    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
    
@@ -36,6 +36,6 @@
    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 
-   <script src="http://localhost/multimarket/app/views/js/pages/datatables.init.js"></script>
+   <script src="<?php echo APP_URL; ?>app/views/js/pages/datatables.init.js"></script>
 
    
